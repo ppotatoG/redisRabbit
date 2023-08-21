bb19d43abb1f2c4f05dc5df28fc6966d86d01a25
ppotatoG <thgus2270@gmail.com>
2023-08-21 22:53:06 +0900

    Add: MongoDB connection timeout

--- backend/src/middleware/db.ts
+++ backend/src/middleware/db.ts
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 export const connectDB = async (MONGO_URI: string) => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGO_URI, {
+      connectTimeoutMS: 10000,
+    });
     console.log('Successfully connected to mongodb');
   } catch (error) {
     console.error('Failed to connect to the database:', error);
